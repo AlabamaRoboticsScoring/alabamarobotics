2b186fe35a7b3eca0c1f12316a766f2f63d96932
Drew Erny <drewerny@gmail.com>
2015-11-24 13:10:55 -0600

    updated basic CRUD backend

--- app/models/attempt.rb
+++ app/models/attempt.rb
@@ -1,4 +1,8 @@
 class Attempt < ActiveRecord::Base
   belongs_to :team
   belongs_to :course
+
+  validates :points, inclusion: 0..100
+  validates :judge_name, presence: true
+
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,13 @@
 class Course < ActiveRecord::Base
+  # attributes:
+  # - pin:string
+  # - name:string
   belongs_to :challenge
   has_many :attempts, dependent: :destroy
+
+  validates :pin, presence: true
+
+  def to_s
+    self.name
+  end
 end
