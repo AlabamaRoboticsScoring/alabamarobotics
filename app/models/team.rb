8aa2760a785b980903e0e4247e39c1f37baf4778
Drew Erny <dperny@crimson.ua.edu>
2015-12-03 02:44:57 -0500

    team now shows score

--- app/models/team.rb
+++ app/models/team.rb
@@ -11,6 +11,16 @@ class Team < ActiveRecord::Base
   validates :school, presence: true
   validates :age_group, inclusion: { in: age_groups }
 
+  def score
+    # in: list of attempts, out: hash of challenges => [attempts]
+    challenges = attempts.group_by {|a| a.course.challenge}
+    # in: challenges => [attempts], out: challenges => attempt
+    maxes = challenges.map{ |challenge, attempts|
+      attempts.sort_by{ |a| [-a.points, a.completion_time] }.first
+    }
+    {total: maxes.reduce(0){ |t,a| t + a.points }, maxes: maxes}
+  end
+
   def to_s 
     self.name
   end
