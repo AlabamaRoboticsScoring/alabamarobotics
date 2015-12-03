a92e8b98f92ac5f4996247d794efc278cb941f39
Drew Erny <dperny@crimson.ua.edu>
2015-12-02 20:06:57 -0500

    judge submitting score now works

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -14,14 +14,42 @@ class SessionController < ApplicationController
 
   # GET /session/attempt
   def new_attempt
+    @attempt = Attempt.new
+    # TODO: autofill judge name
+    # @attempt.juddge_name 
   end
 
   # GET /session/attempt/confirm
   def confirm
+    @attempt = Attempt.new
+
+    @attempt.team_id = params[:team_id]
+    @attempt.course_id = params[:course_id]
+    @attempt.points = params[:points]
+    @attempt.completion_time = params[:completion_time]
+    @attempt.judge_name = params[:judge_name]
+
+    if not @attempt.valid?
+      render :new_attempt
+    end
   end
 
   # POST /session/attempt
   def create_attempt
+    @attempt = Attempt.new
+
+    @attempt.team_id = params[:team_id]
+    @attempt.course_id = params[:course_id]
+    @attempt.points = params[:points]
+    @attempt.completion_time = params[:completion_time]
+    @attempt.judge_name = params[:judge_name]
+    reject = params[:reject].eql? '1'
+
+    if !reject and @attempt.save
+      redirect_to '/session/attempt', notice: 'Attempt was successfully created.'
+    else
+      render 'new_attempt'
+    end
   end
 
   # GET /session/team/1
@@ -31,4 +59,5 @@ class SessionController < ApplicationController
   # GET /session/winners
   def winners
   end
+
 end
